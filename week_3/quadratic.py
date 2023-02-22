236eaa8091d08d2363d9633f8ef4f3124c0f9c95
kmungai80 <kmungai8034@gmail.com>
2023-02-22 11:12:12 +0300

    quadratic

---
+++ week_3/quadratic.py
@@ -0,0 +1,11 @@
+import math
+
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+c=int(input("enter c:"))
+
+sn1= (-b + math.sqrt((math.pow(b,2)- (4*a*c))))/2*a
+sn2= (-b - math.sqrt((math.pow(b,2)- (4*a*c))))/2*a
+
+print("first answer is :",sn1)
+print("second answer is :",sn2)
